da448ae935d37f5afcd2c57308b6c7e259de95f4
Ľudovít Vitárius <luj.vitarius@gmail.com>
2024-05-04 14:04:09 +0200

    Database tables

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
+class Category extends Model
 {
     use HasFactory;
-    public function categories(){
-        return $this->belongsToMany(Category::class)->withTimestamps();
+    public function products(){
+        return $this->belongsToMany(Product::class)->withTimestamps();
     }
 }
