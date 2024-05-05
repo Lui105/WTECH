a5bfad1c3b8708aa282793d7dbcd4cacb0db6ac9
RomanKos08 <kosikroman08@gmail.com>
2024-05-05 11:48:04 +0200

    layour for index page

---
+++ resources/views/components/leftbar_clickpoint.blade.php
@@ -0,0 +1,35 @@
+<div class="col-md-2">
+    <h4>Categories</h4>
+    <div class="list-group mb-3">
+        <a href="#" class="list-group-item list-group-item-action">1st category</a>
+        <a href="#" class="list-group-item list-group-item-action">2nd category</a>
+        <a href="#" class="list-group-item list-group-item-action">3rd category</a>
+        <a href="#" class="list-group-item list-group-item-action">4th category</a>
+        <a href="#" class="list-group-item list-group-item-action">5th category</a>
+        <a href="#" class="list-group-item list-group-item-action">6th category</a>
+        <a href="#" class="list-group-item list-group-item-action">7th category</a>
+        <a href="#" class="list-group-item list-group-item-action">8th category</a>
+        <a href="#" class="list-group-item list-group-item-action">9th category</a>
+        <a href="#" class="list-group-item list-group-item-action">10th category</a>
+
+
+    </div>
+    <div style="max-width: 200px;">
+        <h4>Last viewed</h4>
+        <div class="card mb-2">
+            <img src="https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="Product 1">
+            <div class="card-body">
+                <h6 class="card-title">Product 1</h6>
+                <p class="card-text">55.60€</p>
+            </div>
+        </div>
+        <div class="card mb-2">
+            <img src="https://images.unsplash.com/photo-1615663245857-ac93bb7c39e7?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="card-img-top" alt="Product 1">
+            <div class="card-body">
+                <h6 class="card-title">Product 1</h6>
+                <p class="card-text">55.60€</p>
+            </div>
+        </div>
+    </div>
+
+</div>
---
+++ resources/views/components/pagination_clickpoint.blade.php
@@ -0,0 +1,5 @@
+<div class="row">
+    <div class="col-12 d-flex justify-content-center">
+        {{ $products->links() }}
+    </div>
+</div>
