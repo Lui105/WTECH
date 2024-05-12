d32aebedc2f7682672170328eccf7fd067911ccd
Ľudovít Vitárius <luj.vitarius@gmail.com>
2024-05-12 15:52:23 +0200

    Added user seeder

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'first_name' => 'Admin',
+            'last_name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'phone_number' => '+421903123456',
+            'password' => Hash::make('admin'),
+            'role' => 'admin'
+        ]);
+    }
+}
